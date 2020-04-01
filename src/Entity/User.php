0bb1bb83105577ce4e1c137cbd1b8af23e6b691b
benjamin.pelletier <pelletierbenjamin90@gmail.com>
2020-04-01 11:01:30 +0200

    Create entity User + Migration for DB schema

---
+++ src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $bnb_balance;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getBnbBalance(): ?float
+    {
+        return $this->bnb_balance;
+    }
+
+    public function setBnbBalance(float $bnb_balance): self
+    {
+        $this->bnb_balance = $bnb_balance;
+
+        return $this;
+    }
+}
